f4392bfe7dbc65ef818a06ac410278adea2cfe1d
Otávio Henrique <ohmcruz07@gmail.com>
2023-11-26 20:35:32 -0300

    filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -20,10 +20,28 @@ app.use(express.json())
 
 app.post('/completar', (requisicao, resposta) => {
     const id = requisicao.body.id
-    
+
     const sql = `
         UPDATE tarefas
         SET completa = '1'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
+app.post('/descompletar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '0'
         WHERE id = ${id}
     `
 
@@ -71,7 +89,13 @@ app.get('/', (requisicao, resposta) => {
             }
         })
 
-        resposta.render('home', { tarefas })
+        const tarefasAtivas = tarefas.filter((tarefa) => {
+            return tarefa.completa === false && tarefa
+        })
+
+        const quantidadeTarefasAtivas = tarefasAtivas.length
+
+        resposta.render('home', { tarefas, quantidadeTarefasAtivas })
     })
 })
 
